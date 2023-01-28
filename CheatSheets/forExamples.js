bafb9ecfe5ecfab5d530f487d2b5e2489c446ce7
Petya Todorova <invisioncl@gmail.com>
2023-01-28 12:02:23 +0200

    forExamples correction

--- CheatSheets/forExamples.js
+++ CheatSheets/forExamples.js
@@ -1,19 +1,19 @@
 // for...in example
 function forExamples() {
-    // // Objects
-    // let obj = { "name": "Jake", "level": 1000, "items": ["Gauss", "HolidayGrenade"] };
-    // for (const key in obj) {
-    //     // property
-    //     console.log(key);
+    // Objects
+    let obj = { "name": "Jake", "level": 1000, "items": ["Gauss", "HolidayGrenade"] };
+    for (const key in obj) {
+        // property
+        console.log(key);
 
-    //     // value
-    //     console.log(obj[key]);
-    // }
+        // value
+        console.log(obj[key]);
+    }
 
-    // // hasOwnProperty example
-    // console.log(obj.hasOwnProperty('name'));
+    // hasOwnProperty example
+    console.log(obj.hasOwnProperty('name'));
 
-    // // ****************************************************************
+    // ****************************************************************
     // Arrays
     let arr = [22, 29, 39];
 
