90c422ba09eef79871e0eabee66e4998894d801c
Petya Todorova <invisioncl@gmail.com>
2023-02-09 21:19:41 +0200

    More from DOM basics.

--- 04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/04. Search in List/search.js
+++ 04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/04. Search in List/search.js
@@ -1,3 +1,19 @@
 function search() {
-   // TODO
+   let listElements = document.querySelectorAll('ul#towns li');
+   let resultElement = document.getElementById('result');
+   let searchText = document.getElementById('searchText').value;
+   let matches = 0;
+   for (let cleanElement of listElements) {
+      cleanElement.style.fontWeight = 'normal';
+      cleanElement.style.textDecoration = '';
+   }
+   for (let element of listElements) {
+      let text = element.textContent;
+      if (text.match(searchText)) {
+         matches++;
+         element.style.fontWeight = 'bold';
+         element.style.textDecoration = 'underline';
+      }
+   }
+   resultElement.textContent = `${matches} matches found`;
 }
--- 04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/06. Format the Text/solution.js
+++ 04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/06. Format the Text/solution.js
@@ -1,3 +1,14 @@
 function solve() {
-  //TODO
+  let input = document.getElementById('input').value;
+  let output = document.getElementById('output');
+
+  let sentences = input.split('.').filter(s => s.length != 0);
+
+  debugger
+  while (sentences.length > 0) {
+    let textForParagraph = sentences.splice(0, 3).join('. ') + '.';
+    let p = document.createElement('p');
+    p.textContent = textForParagraph;
+    output.appendChild(p);
+  }
 }
